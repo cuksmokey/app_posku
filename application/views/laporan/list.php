1ceedcf57e0c82a045a654d0551f7def74b6b81a
cuksmokey <bolongs360@gmail.com>
2023-01-24 12:04:43 +0700

    penyesuain total transaksi laporan

--- application/views/laporan/list.php
+++ application/views/laporan/list.php
@@ -30,30 +30,68 @@
                             <tr>
                                 <th style="text-align:center">No.</th>
                                 <th style="text-align:center">ID Transaksi</th>
-                                <th style="text-align:center">Tanggal Transaksi</th>
+                                <th style="text-align:center">Tgl Transaksi</th>
                                 <th style="text-align:center">Operator</th>
                                 <th style="text-align:center">Total Transaksi</th>
+                                <th style="text-align:center">Total Diskon</th>
+                                <th style="text-align:center">Total Bayar</th>
                                 <th style="text-align:center">Nota</th>
                             </tr>
                         </thead>
                         <tbody>
-                        <?php $no=1; $total=0; foreach ($record->result() as $r){ ?>
+                        <?php
+							$no = 0;
+							$toTotal = 0;
+							$totDis = 0;
+							$totSubTotal = 0;
+							foreach ($record->result() as $r) {
+								$no++;
+								$idt = $this->db->query("SELECT*FROM transaksi_dtl WHERE id_transaksi='$r->id_transaksi'");
+								$totTransaksi = 0;
+								$sumSubTotal = 0;
+								$subDis = 0;
+								foreach($idt->result() as $r2){
+									if($r2->discrp2 == 0){
+										$subTotal = $r2->qty * $r2->harga;
+										$jumdis = 0;
+									}else{
+										if($r2->disc == 0){
+											$subTotal = (($r2->qty * $r2->harga) - $r2->potongan);
+											$jumdis = $r2->potongan;
+										}else{
+											$subTotal = ($r2->qty * $r2->harga) - $r2->discrp2;
+											$jumdis = $r2->discrp2;
+										}
+									}
+									$totTransaksi += $r2->qty * $r2->harga;
+									$sumSubTotal += $subTotal;
+									$subDis += $jumdis;
+								}
+
+							$toTotal += $totTransaksi;
+							$totDis += $subDis;
+							$totSubTotal += $sumSubTotal;
+						?>
                             <tr class="gradeU">
-                                <td><?= $no ?></td>
+                                <td style="text-align:center"><?= $no ?></td>
                                 <td><?= $r->invoice ?></td>
                                 <td><?= $this->M_cetak->tanggal_format_indonesia($r->tanggal_transaksi) ?></td>
                                 <td><?= $r->nama_lengkap ?></td>
-                                <td style="text-align:right">Rp. <?= number_format($r->total) ?></td>
+                                <td style="text-align:right">Rp. <?= number_format($totTransaksi) ?></td>
+                                <td style="text-align:right">Rp. <?= number_format($subDis) ?></td>
+                                <td style="text-align:right">Rp. <?= number_format($sumSubTotal) ?></td>
                                 <td style="text-align:center">
                                     <button type="button" onclick="cetak_nota('<?= $r->id_transaksi ?>')" class="btn btn-warning"><b>
                                         <i class="fa fa-print"></i>&nbsp; Cetak</b>
                                     </button>
                                 </td>
                             </tr>
-                        <?php $no++; $total=$total+ $r->total; } ?>
+                        <?php } ?>
                             <tr>
-                                <td colspan="4">Total</td>
-                                <td style="text-align:right">Rp. <?= number_format($total);?></td>
+                                <td style="text-align:center;font-weight:bold" colspan="4">Total</td>
+                                <td style="text-align:right;font-weight:bold">Rp. <?= number_format($toTotal);?></td>
+                                <td style="text-align:right;font-weight:bold">Rp. <?= number_format($totDis);?></td>
+                                <td style="text-align:right;font-weight:bold">Rp. <?= number_format($totSubTotal);?></td>
                                 <td></td>
                             </tr>
                         </tbody>
